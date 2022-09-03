e920554e6a49768f26b54b64badf799746f0226d
kelly-tou <97404518+kelly-tou@users.noreply.github.com>
2022-09-03 08:41:00 -0700

    Update CartViewModel.swift

--- ViewModel/CartViewModel.swift
+++ ViewModel/CartViewModel.swift
@@ -3,8 +3,33 @@ import SwiftUI
 class CartViewModel: ObservableObject{
     @Published var openAddItem: Bool = false
     @Published var items = [Item]()
+
+    {
+        didSet {
+            saveItems()
+        }
+    }
+
+    let itemsKey: String = "items_list"
+
+    init(){
+        getItems()
+    }
+
+    func getItems(){
+        guard
+            let data = UserDefaults.standard.data(forKey: itemsKey), let savedItems = try? JSONDecoder().decode([Item].self, from: data)
+        else { return }
+        self.items = savedItems
+    }
     
     func addItem(name: String, price: Float){
         items.append(Item(name: name, price: price, quantity: 0, offset: 0, isSwiped: false))
+    }
+
+    func saveItems() {
+        if let encodedData = try? JSONEncoder().encode(items) {
+            UserDefaults.standard.set(encodedData, forKey: itemsKey)
+        }
     }
 }
