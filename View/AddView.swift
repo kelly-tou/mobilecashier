1943804fca5aa5b1ad401dcb3586593d558fb2a0
kelly-tou <97404518+kelly-tou@users.noreply.github.com>
2022-09-03 21:10:32 -0700

    Update AddView.swift

--- View/AddView.swift
+++ View/AddView.swift
@@ -20,6 +20,7 @@ struct AddView: View {
                             .foregroundColor(.black)
                     }
                 }
+                .padding(.bottom)
             VStack(alignment: .leading, spacing: 12) {
                 Text("Item Name")
                     .font(.caption)
@@ -34,31 +35,62 @@ struct AddView: View {
                 Text("Item Price")
                     .font(.caption)
                     .foregroundColor(.gray)
-                TextField("", text: $itemPrice)
-                    .frame(maxWidth: .infinity)
-                    .padding(.top, 10)
+                HStack{
+                    Text("$")
+                        .font(.headline)
+                        .foregroundColor(.black)
+                        .padding(.top, 10)
+                    
+                    TextField("", text: $itemPrice)
+                        .frame(maxWidth: .infinity)
+                        .padding(.top, 10)
+                        .keyboardType(.decimalPad)
+                }
             }
             Divider()
             
             Button{
-                cartViewModel.addItem(name: itemName, price: Float(itemPrice) ?? 0)
-                presentationMode.wrappedValue.dismiss()
+                saveButtonPressed()
             } label: {
                 Text("Save")
                     .font(.callout)
                     .fontWeight(.semibold)
                     .frame(maxWidth: .infinity)
-                    .padding(.vertical, 12)
+                    .padding(12)
                     .foregroundColor(.white)
-                    .background{
-                        Capsule()
-                    }
+                    .background(Color(red: 255 / 255, green: 123 / 255, blue: 0 / 255))
+                    .cornerRadius(10)
             }
             .frame(maxHeight: .infinity, alignment: .bottom)
             .padding(.bottom, 10)
         }
         .frame(maxHeight: .infinity, alignment: .top)
         .padding()
+    }
+    
+    func saveButtonPressed() {
+        if isNameValid() && isPriceValid() {
+            cartViewModel.addItem(name: itemName, price: Float(itemPrice) ?? 0)
+            presentationMode.wrappedValue.dismiss()
+        }
+    }
+    
+    func isNameValid() -> Bool {
+        if itemName.isEmpty {
+            return false
+        }
+        return true
+    }
+    
+    func isPriceValid() -> Bool {
+        for char in itemPrice {
+            let scalarValues = String(char).unicodeScalars
+            let charAscii = scalarValues[scalarValues.startIndex].value
+            if charAscii < 48 || charAscii > 57 {
+                return false
+            }
+        }
+        return true
     }
 }
 
